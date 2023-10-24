bd33404a1b66ac9964d04d0892603bbb83c2cb2b
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2023-10-23 17:24:40 -0700

    first commit

---
+++ StringMaker_Ritola/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SillyStackTests
+{
+
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			StringManager myStringMaker = new StringManager();
+
+			// Test 1: Reverse a string
+			Console.WriteLine(myStringMaker.Reverse("Sunbeam Tiger"));
+
+			// Test 2: ToString()
+			Console.WriteLine(myStringMaker.ToString());
+
+			// Test 3: Reverse a string preserving casing location
+			Console.WriteLine(myStringMaker.Reverse("Sunbeam Tiger", true));
+
+			// Test 4: Equality check
+			Console.WriteLine(myStringMaker.Equals("Sunbeam Tiger"));
+
+			// Test 5: Symmetric check
+			Console.WriteLine(myStringMaker.Symmetric("ABBA"));
+			Console.WriteLine(myStringMaker.Symmetric("ABA"));
+			Console.WriteLine(myStringMaker.Symmetric("ABba"));
+
+
+		}
+	}
+}
---
+++ StringMaker_Ritola/StringManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SillyStackTests
+{
+	public class StringManager
+	{
+		private string originalInput = string.Empty;
+
+		public string Reverse(string s)
+		{
+			originalInput = s;
+			Stack<char> stack = new Stack<char>();
+			foreach (char c in s)
+			{
+				stack.Push(c);
+			}
+
+			StringBuilder reversed = new StringBuilder();
+			while (stack.Count > 0)
+			{
+				reversed.Append(stack.Pop());
+			}
+
+			return reversed.ToString();
+		}
+
+		public string Reverse(string s, bool preserveCaseLocation)
+		{
+			originalInput = s;
+			Stack<char> stack = new Stack<char>();
+			Queue<int> caseLocations = new Queue<int>();
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				char c = s[i];
+				stack.Push(c);
+
+				if (char.IsLetter(c))
+				{
+					caseLocations.Enqueue(i);
+				}
+			}
+
+			StringBuilder reversed = new StringBuilder();
+			while (stack.Count > 0)
+			{
+				char c = stack.Pop();
+				if (preserveCaseLocation && char.IsLetter(c))
+				{
+					int location = caseLocations.Dequeue();
+					reversed.Insert(location, c);
+				}
+				else
+				{
+					reversed.Append(c);
+				}
+			}
+
+			return reversed.ToString();
+		}
+
+		public bool Symmetric(string s)
+		{
+			return s.Equals(Reverse(s));
+		}
+
+		public override string ToString()
+		{
+			if (originalInput.Length == 0)
+			{
+				return "Negative One";
+			}
+
+			StringBuilder result = new StringBuilder();
+			foreach (char c in originalInput)
+			{
+				result.Append($"{(int)c} ");
+			}
+
+			return result.ToString().Trim();
+		}
+
+
+		public override bool Equals(object obj)
+		{
+			if (obj is string str)
+			{
+				return originalInput.Equals(str);
+			}
+
+			return false;
+		}
+	}
+}
